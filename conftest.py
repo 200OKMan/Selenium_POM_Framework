87e8b7a53cab8aa5cb5eb064baa928326beeb0b2
Mark <notformajormail@gmail.com>
2025-11-06 03:12:53 -0600

    Cross-browser support added.

--- conftest.py
+++ conftest.py
@@ -1,40 +1,54 @@
 import os
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+
 
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name", action="store", default="chrome", help="browser selection"
     )
 
+
 @pytest.fixture(scope="function")
 def browser_configuration(request):
     browser_name = request.config.getoption("browser_name")
     driver = None
 
-    if browser_name == "chrome":
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument("window-size=1920,1080")
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--incognito")
-        chrome_options.add_argument("--disable-sync")
-        chrome_options.add_argument("--disable-popup-blocking")
-        driver = webdriver.Chrome(options=chrome_options)
 
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        options.add_argument("--headless=new")
+        options.add_argument("window-size=1920,1080")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--incognito")
+        options.add_argument("--disable-sync")
+        options.add_argument("--disable-popup-blocking")
+
+        driver = webdriver.Chrome(options=options)
+
+    # --- Firefox ---
     elif browser_name == "firefox":
-        driver = webdriver.Firefox()
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        options.add_argument("--width=1920")
+        options.add_argument("--height=1080")
+
+        driver = webdriver.Firefox(options=options)
+
     else:
-        raise ValueError("Unsupported browser")
+        raise ValueError(f"Unsupported browser: {browser_name}")
 
     request.node.driver = driver
-
     yield driver
-
     driver.quit()
 
 @pytest.hookimpl(hookwrapper=True)
