3d165f15069c84f8e4174472efa23f81e3191170
mark <notformajormail@gmail.com>
2025-11-21 22:58:50 -0600

    Added Edge and Safari support.

--- conftest.py
+++ conftest.py
@@ -1,12 +1,10 @@
 import os
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.safari.options import Options as SafariOptions
 
 
 def pytest_addoption(parser):
@@ -20,11 +18,10 @@ def browser_configuration(request):
     browser_name = request.config.getoption("browser_name")
     driver = None
 
-
     if browser_name == "chrome":
         options = ChromeOptions()
         options.add_argument("--headless=new")
-        options.add_argument("window-size=1920,1080")
+        options.add_argument("--window-size=1920,1080")
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
@@ -32,24 +29,35 @@ def browser_configuration(request):
         options.add_argument("--incognito")
         options.add_argument("--disable-sync")
         options.add_argument("--disable-popup-blocking")
-
         driver = webdriver.Chrome(options=options)
 
-    # --- Firefox ---
     elif browser_name == "firefox":
         options = FirefoxOptions()
         options.add_argument("--headless")
         options.add_argument("--width=1920")
         options.add_argument("--height=1080")
-
         driver = webdriver.Firefox(options=options)
+
+    elif browser_name == "edge":
+        options = EdgeOptions()
+        options.add_argument("--headless")
+        options.add_argument("window-size=1920,1080")
+        driver = webdriver.Edge(options=options)
+
+    elif browser_name == "safari":
+        options = SafariOptions()
+        driver = webdriver.Safari(options=options)
+        driver.set_window_size(1920, 1080)
 
     else:
         raise ValueError(f"Unsupported browser: {browser_name}")
 
     request.node.driver = driver
     yield driver
-    driver.quit()
+
+    if driver is not None:
+        driver.quit()
+
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item):
@@ -58,29 +66,35 @@ def pytest_runtest_makereport(item):
     report = outcome.get_result()
     extra = getattr(report, "extra", [])
 
-    if report.when in ("call", "setup"):
+    if report.when == "call":
         xfail = hasattr(report, "wasxfail")
         if (report.skipped and xfail) or (report.failed and not xfail):
 
-            reports_dir = os.path.join(os.path.dirname(__file__), "reports")
-            if not os.path.exists(reports_dir):
-                os.makedirs(reports_dir)
+            if hasattr(item, "driver") and item.driver:
+                reports_dir = os.path.join(os.path.dirname(__file__), "reports")
+                if not os.path.exists(reports_dir):
+                    os.makedirs(reports_dir)
 
-            file_name = os.path.join(
-                reports_dir, report.nodeid.replace("::", "_") + ".png"
-            )
-            print("Saving screenshot to: " + file_name)
+                file_name = report.nodeid.replace("::", "_") + ".png"
+                file_name = file_name.replace("/", "_").replace("\\", "_")
 
-            if hasattr(item, "driver"):
-                _capture_screenshot(item.driver, file_name)
+                destination_file = os.path.join(reports_dir, file_name)
 
-            if file_name:
-                html = (
-                    '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" '
-                    'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                )
-                extra.append(pytest_html.extras.html(html))
+                try:
+                    _capture_screenshot(item.driver, destination_file)
+
+                    if destination_file:
+                        html = (
+                            f'<div><img src="{file_name}" alt="screenshot" '
+                            'style="width:304px;height:228px;" '
+                            'onclick="window.open(this.src)" align="right"/></div>'
+                        )
+                        extra.append(pytest_html.extras.html(html))
+                except Exception:
+                    pass
+
         report.extras = extra
+
 
 def _capture_screenshot(driver, file_name):
     driver.get_screenshot_as_file(file_name)
